9a7c3c5cf210276a74f6280cbae663d7ac214c12
Andrzej Lichaczewski <andrewlee224@gmail.com>
2015-09-17 23:26:40 +0200

    Added field of view constants; header include guards

--- variables.h
+++ variables.h
@@ -1,3 +1,6 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/ext.hpp"
@@ -10,7 +13,10 @@ typedef glm::vec4 vec4;
 
 const int WIDTH = 320;
 const int HEIGHT = 240;
+const int FOVX = 160;
+const int FOVY = 155;
 const int BPP = 24;
 
 // for float operation approximations
 const float EPSILON = 0.0001;
+#endif
